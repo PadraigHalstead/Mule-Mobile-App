43873dbc7fef321a515ccec26a0a619760c4a284
Padraig <padraighalstead@gmail.com>
2023-03-27 00:52:10 +0100

    Complete app

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { HomeScreen } from './screens/HomeScreen'; 
+import { SignInScreen } from './screens/SignInScreen';
+import { ValidationScreen } from './screens/ValidationScreen'; 
+import { QrScannerScreen } from './screens/QrScannerScreen'
 
-export default function App() {
+
+
+
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Sign In"
+        component={SignInScreen}
+        options={{title: 'Welcome'}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{title: 'Home'}}
+      />
+      <Stack.Screen
+        name="QrScanner"
+        component={QrScannerScreen}
+        options={{title: 'Scan a QR Code'}}
+      />
+      <Stack.Screen
+        name="Validation"
+        component={ValidationScreen}
+        options={{title: 'Validation'}}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+    
+
+export default App;
