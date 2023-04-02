bdf54e9b819fbcace7dc91da2b489278f4f38aac
Padraig Halstead <79229057+PadraigHalstead@users.noreply.github.com>
2023-04-02 13:42:19 +0100

    Update ValidationScreen.js

--- screens/ValidationScreen.js
+++ screens/ValidationScreen.js
@@ -1,111 +1,99 @@
 import * as React from 'react';
-import { View, Text,Pressable} from 'react-native'
+import { View, Text, Pressable } from 'react-native';
 import { db } from '../config/firebase';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
 import { styles } from '../styles';
 
-export const ValidationScreen = ({navigation, route}) => {
-    const [studentEmail, setStudentEmail] = useState("")
-    const [studentModuleCode, setStudentModuleCode] = useState("")
-    const [userUID, setUserUID] = useState(route.params.uid)
-    const [validated, setValidated] = useState(false)
-    const [validQR, setValidQR] = useState(false)
+export const ValidationScreen = ({ navigation, route }) => {
+  const [studentEmail, setStudentEmail] = useState('');
+  const [studentModuleCode, setStudentModuleCode] = useState('');
+  const [userUID, setUserUID] = useState(route.params.uid);
+  const [validated, setValidated] = useState(false);
+  const [validQR, setValidQR] = useState(true);
 
-    useEffect(() => {
-      getStudentDetails();
-    }, [])
-    
+  useEffect(() => {
+    getStudentDetails();
+  }, []);
 
-    const getStudentDetails = async () => {
-      try{
-        const userDataRef = doc(db, "users", userUID)
-        const userSnapshot = await getDoc(userDataRef)
-        const currentUserData = userSnapshot.data()
-        if(currentUserData.exists()){
-          setStudentEmail(currentUserData.email)
-          setStudentModuleCode(currentUserData.module)
-          setValidated(currentUserData.validated)
-        } else {
-          setValidQR(false)
-        }
-      } catch (err) {
-        setValidQR(false)
-      }
-      
-    }
-
-    const handleAccept = async () => {
-        const selectedStudentRef = doc(db, "users", userUID)
-    try{
-      await updateDoc(selectedStudentRef, {validated: true});
+  const getStudentDetails = async () => {
+    try {
+      const userDataRef = doc(db, 'users', userUID);
+      const userSnapshot = await getDoc(userDataRef);
+      const currentUserData = userSnapshot.data();
+      setStudentEmail(currentUserData.email);
+      setStudentModuleCode(currentUserData.module);
+      setValidated(currentUserData.validated);
     } catch (err) {
-      console.error(err)
+      setValidQR(false);
+    }
+  };
+
+  const handleAccept = async () => {
+    const selectedStudentRef = doc(db, 'users', userUID);
+    try {
+      await updateDoc(selectedStudentRef, { validated: true });
+    } catch (err) {
+      console.error(err);
     }
     navigateToHomePage();
-    }
+  };
 
-    const navigateToHomePage = () => {
-        navigation.replace('Home');
-    }
+  const navigateToHomePage = () => {
+    navigation.replace('Home');
+  };
 
-    const InvalidQR = () => {
-      return (
-        <View>
-          <Text style={styles.heading4}>Invalid QR / User does not exist</Text>
-            <Text/>
+  const InvalidQR = () => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading4}>Invalid QR / User does not exist</Text>
+        <Text />
+
+        <Pressable style={styles.button} onPress={navigateToHomePage}>
+          <Text style={styles.text}>Okay</Text>
+        </Pressable>
+      </View>
+    );
+  };
+
+  const ValidQR = () => {
+    return (
+      <View style={styles.container}>
+        {validated ? (
+          <View>
+            <Text style={styles.heading4}>Student Already Validated</Text>
+            <Text />
 
             <Pressable style={styles.button} onPress={navigateToHomePage}>
-            <Text style={styles.text}>Okay</Text>
-            </Pressable>
-        </View>
-      )
-    }
-
-    const ValidQR = () => {
-      return (
-        <View style={styles.container}>
-          {validated ? (
-            <View>
-              <Text style={styles.heading4}>Student Already Validated</Text>
-              <Text/>
-
-              <Pressable style={styles.button} onPress={navigateToHomePage}>
               <Text style={styles.text}>Okay</Text>
-              </Pressable>
-            </View>
-          ): (
-            <View>
-              <Text style={styles.heading3}>Student Details:</Text>
-            <Text/> 
+            </Pressable>
+          </View>
+        ) : (
+          <View style={styles.container}>
+            <Text style={styles.heading3}>Student Details:</Text>
+            <Text />
             <Text style={styles.heading4}>Email: {studentEmail}</Text>
-            <Text/> 
+            <Text />
             <Text style={styles.heading4}>Module: {studentModuleCode}</Text>
-            <Text/> 
+            <Text />
 
             <Pressable style={styles.button} onPress={handleAccept}>
-            <Text style={styles.text}>Accept</Text>
+              <Text style={styles.text}>Accept</Text>
             </Pressable>
 
-            <Text/>
+            <Text />
 
             <Pressable style={styles.button} onPress={navigateToHomePage}>
-            <Text style={styles.text}>Deny</Text>
+              <Text style={styles.text}>Deny</Text>
             </Pressable>
-            </View>
-
-          )}  
-        </View>
-    )
-  }
+          </View>
+        )}
+      </View>
+    );
+  };
   return (
-    <View>
-    {validQR ? (
-      <ValidQR/>
-    ) : (
-      <InvalidQR/>
-    )}
-    </View>   
-  )
-    
-}
+    <View style={styles.container}>
+      {validQR ? <ValidQR /> : <InvalidQR />}
+    </View>
+  );
+};
